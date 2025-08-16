3af0c404cb32011176cefb4c951917af2e335809
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-08-16 15:04:06 +0530

    feat: enhance SignUp component with form submission and user feedback

--- task-app/src/components/SignUp.jsx
+++ task-app/src/components/SignUp.jsx
@@ -1,15 +1,43 @@
 import { User } from "lucide-react";
 import { useState } from "react";
 import {
+  BUTTONCLASSES,
   FIELDS,
   Inputwrapper,
   MESSAGE_ERROR,
   MESSAGE_SUCCESS,
 } from "../assets/dummy";
+import axios from "axios";
 const API_URL = "http://localhost:5173/";
 const INITIAL_FORM = { name: "", email: "", password: "" };
 
-const SignUp = () => {
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setLoading(true);
+  setMessage({ text: "", type: "" });
+
+  try {
+    const data = await axios.post(`${API_URL}/api/user/register`, formData);
+    console.log("signup successful", data);
+    setMessage({
+      text: "Registration successful! Please log in.",
+      type: "success",
+    });
+    setFormData(INITIAL_FORM);
+  } catch (error) {
+    console.error("signup failed", error);
+    setMessage({
+      text:
+        error.response?.data?.message ||
+        "Registration failed. Please try again.",
+      type: "error",
+    });
+  } finally {
+    setLoading(false);
+  }
+};
+
+const SignUp = ({ onSwitchMode }) => {
   const [formData, setFormData] = useState(INITIAL_FORM);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState({ text: "", type: "" });
@@ -37,9 +65,40 @@ const SignUp = () => {
         {FIELDS.map(({ name, type, placeholder, icon: Icon }) => {
           <div key={name} className={Inputwrapper}>
             <Icon className="text-purple-500 w-5 h-5 mr-2" />
+            <input
+              type={type}
+              placeholder={placeholder}
+              value={formData[name]}
+              onChange={(e) =>
+                setFormData({ ...formData, [name]: e.target.value })
+              }
+              className="w-full focus:outline-none text-sm text-gray-700"
+              required
+            />
           </div>;
         })}
+
+        <button type="submit" className={BUTTONCLASSES} disabled={loading}>
+          {loading ? (
+            "Signing up..."
+          ) : (
+            <>
+              <UserPlus className="w-4 h-4" />
+              Sign Up
+            </>
+          )}
+        </button>
       </form>
+
+      <p className="text-center  text-sm text-gray-600 mt-6">
+        Already have an account?{" "}
+        <button
+          onClick={onSwitchMode}
+          className="text-purple-600 hover:text-purple-700 hover:underline font-medium transition-colors"
+        >
+          Log In
+        </button>
+      </p>
     </div>
   );
 };
