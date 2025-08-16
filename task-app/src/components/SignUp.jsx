e4facd9db9ba970e293c53c8551feee88f551107
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-08-16 14:23:45 +0530

    feat: implement SignUp component with form handling and user feedback

--- task-app/src/components/SignUp.jsx
+++ task-app/src/components/SignUp.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { User } from "lucide-react";
+import { useState } from "react";
+import {
+  FIELDS,
+  Inputwrapper,
+  MESSAGE_ERROR,
+  MESSAGE_SUCCESS,
+} from "../assets/dummy";
+const API_URL = "http://localhost:5173/";
+const INITIAL_FORM = { name: "", email: "", password: "" };
 
 const SignUp = () => {
-  return <div></div>;
+  const [formData, setFormData] = useState(INITIAL_FORM);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState({ text: "", type: "" });
+  return (
+    <div className="max-w-md w-full bg-white shadow-lg border border-purple-100 rounded-xl p-8">
+      <div className="mb-6 text-center">
+        <div className="w-16 h-16 bg-gradient-to-br from-fuchsia-500 to-purple-600 rounded-full mx-auto flex items-center justify-center mb-4">
+          <User className="w-8 h-8 text-white" />
+        </div>
+        <h2 className="text-2xl font-bold text-gray-800">Create Account</h2>
+        <p className="text-gray-500 text-sm mt-1">
+          Join TaskFlow to manage your tasks efficiently.
+        </p>
+      </div>
+      {message.text && (
+        <div
+          className={
+            message.type === "success" ? MESSAGE_SUCCESS : MESSAGE_ERROR
+          }
+        >
+          {message.text}
+        </div>
+      )}
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {FIELDS.map(({ name, type, placeholder, icon: Icon }) => {
+          <div key={name} className={Inputwrapper}>
+            <Icon className="text-purple-500 w-5 h-5 mr-2" />
+          </div>;
+        })}
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
