f2d13a2f09192f689e5096df2af641b1baad1853
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-08-19 17:47:00 +0530

    mongodb cloud add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/TaskFlow").then(() => {
-    console.log("MongoDB connected successfully");
-  });
+  await mongoose
+    .connect(
+      "mongodb+srv://janadajayathilaka123:Im7pk3ohIj07gfK1@cluster0.hyp74ny.mongodb.net/"
+    )
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    });
 };
