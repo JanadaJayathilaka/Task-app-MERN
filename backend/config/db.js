dfadd11264dc80cb4a118cd1007d524e9e0a5bfe
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-08-19 18:14:54 +0530

    fix: update MongoDB connection string to include database name and options

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://janadajayathilaka123:Im7pk3ohIj07gfK1@cluster0.hyp74ny.mongodb.net/"
+      "mongodb+srv://janadajayathilaka123:Im7pk3ohIj07gfK1@cluster0.hyp74ny.mongodb.net/TaskFlow?retryWrites=true&w=majority"
     )
     .then(() => {
       console.log("MongoDB connected successfully");
